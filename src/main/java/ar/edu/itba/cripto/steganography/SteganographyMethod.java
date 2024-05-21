17b3a33c744e93d0c3ae350410dd0b02a746cc86
Gonzalo Martone <gmartone@itba.edu.ar>
2024-05-21 19:40:13 -0300

    chore: set template for steganography methods

--- src/main/java/ar/edu/itba/cripto/steganography/SteganographyMethod.java
+++ src/main/java/ar/edu/itba/cripto/steganography/SteganographyMethod.java
@@ -1,7 +1,42 @@
 package ar.edu.itba.cripto.steganography;
 
+import ar.edu.itba.cripto.model.BMP;
+
 public enum SteganographyMethod {
-    LSB1,
-    LSB4,
-    LSBI
+    LSB1{
+        @Override
+        public BMP embed(Byte[] message, BMP image) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public Byte[] extract(BMP image) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    },
+    LSB4{
+        @Override
+        public BMP embed(Byte[] message, BMP image) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public Byte[] extract(BMP image) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    },
+    LSBI{
+        @Override
+        public BMP embed(Byte[] message, BMP image) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public Byte[] extract(BMP image) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    };
+
+    public abstract BMP embed(Byte[] message, BMP image);
+    public abstract Byte[] extract(BMP image);
 }
