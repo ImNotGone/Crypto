7c70644be199ecb289b347fce28c465103de9545
Gonzalo Alfredo Martone <gmartone@itba.edu.ar>
2024-05-22 20:09:39 -0300

    refactor: restructure BMP

--- src/main/java/ar/edu/itba/cripto/model/BMP.java
+++ src/main/java/ar/edu/itba/cripto/model/BMP.java
@@ -5,66 +5,67 @@ import java.io.IOException;
 
 public class BMP {
     private int width;
-    private int rowSize;
     private int height;
+    private int rowSize;
     private byte[] pixelData;
 
     // only working with 24 bit BMPs
-    private final int BYTES_PER_PIXEL = 3;
-
-    // BMP Header fields
-    private int fileSize;
-    private int dataOffset;
-    private int headerSize;
-    private int bitsPerPixel;
-    private int compression;
+    // Source: https://en.wikipedia.org/wiki/BMP_file_format#Example_1
+    private static final int BYTES_PER_PIXEL = 3;
+    private static final int FILE_HEADER_SIZE = 14;
+    private static final int BMP_INFO_HEADER_SIZE = 40;
+    private static final int HEADER_SIZE = FILE_HEADER_SIZE + BMP_INFO_HEADER_SIZE;
+    private static final int BITS_PER_PIXEL = BYTES_PER_PIXEL * 8;
+    private static final int COMPRESSION = 0;
 
     public BMP(String filePath) throws IOException {
         loadBMP(filePath);
     }
 
-    // Source: https://en.wikipedia.org/wiki/BMP_file_format#Example_1
+    public BMP(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.rowSize = getRowSize(width);
+        this.pixelData = new byte[height * rowSize];
+    }
+
+    private int getRowSize(int width) {
+        return (width * BYTES_PER_PIXEL + 3) & ~3;
+    }
+
     private void loadBMP(String filePath) throws IOException {
         FileInputStream fis = new FileInputStream(filePath);
 
         // BMP Header
-        byte[] header = new byte[54];
-        if(fis.read(header, 0, 54) != 54) {
+        byte[] header = new byte[HEADER_SIZE];
+        if(fis.read(header, 0, HEADER_SIZE) != HEADER_SIZE) {
             throw new IOException("Invalid BMP file");
-        };
+        }
 
         // Check for BM marker
         if (header[0] != 'B' || header[1] != 'M') {
             throw new IOException("Not a valid BMP file");
         }
 
-        fileSize = readInt(header, 2);
-        dataOffset = readInt(header, 10);
-        headerSize = readInt(header, 14);
-        width = readInt(header, 18);
-        height = readInt(header, 22);
-        bitsPerPixel = readShort(header, 28);
-        compression = readInt(header, 30);
+        this.width = readInt(header, 18);
+        this.height = readInt(header, 22);
+        int bitsPerPixel = readShort(header, 28);
+        int compression = readInt(header, 30);
 
-        // Solo soportamos bmp con 24 bits por pixel
-        if (bitsPerPixel != 24) {
-            throw new IOException("Only 24-bit BMP files are supported");
-        }
-
-        // Solo soportamos bmp sin compresion
-        if(compression != 0) {
-            throw new IOException("Only BMP files without compression are supported");
+        // Solo soportamos bmp con 24 bits por pixel sin compresion
+        if (bitsPerPixel != BITS_PER_PIXEL || compression != COMPRESSION) {
+            throw new IOException("Only 24-bit BMP files without compression are supported");
         }
 
         // multiplico x BYTES_PER_PIXEL y agrego BYTES_PER_PIXEL
         // luego hago & con 11...1100
         // para alinear al siguiente multiplo de 4
-        rowSize = (width * BYTES_PER_PIXEL + BYTES_PER_PIXEL) & ~3;
+        rowSize = getRowSize(width);
         pixelData = new byte[rowSize * height];
 
+        int dataOffset = readInt(header, 10);
         fis.skip(dataOffset - 54);
         fis.read(pixelData);
-
         fis.close();
     }
 
