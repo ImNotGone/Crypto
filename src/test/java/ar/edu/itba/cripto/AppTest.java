515eb6a5571ffa3fc940d3223e4d4ffc186a7857
Santiago Ballerini <sballerini@itba.edu.ar>
2024-06-19 19:44:16 -0300

    feat: add steganography with encription tests

--- src/test/java/ar/edu/itba/cripto/AppTest.java
+++ src/test/java/ar/edu/itba/cripto/AppTest.java
@@ -1,80 +1,112 @@
 package ar.edu.itba.cripto;
 
+import ar.edu.itba.cripto.cryptography.Cryptography;
+import ar.edu.itba.cripto.cryptography.CryptographyAlgorithm;
+import ar.edu.itba.cripto.cryptography.CryptographyMode;
 import ar.edu.itba.cripto.steganography.Embed;
 import ar.edu.itba.cripto.steganography.Extract;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.stream.Stream;
 
 public class AppTest {
 
-    private static final String coverFilePath = "src/test/resources/cover.bmp";
-    private static final String messageFilePath = "src/test/resources/message.java";
+    private static final String COVER_FILE_PATH = "src/test/resources/cover.bmp";
+    private static final String MESSAGE_FILE_PATH = "src/test/resources/message.java";
+    private static final String PASSWORD = "stegobmp";
+
+    private static final String[] SUPPORTED_STEGANOGRAPHY_METHODS = {"LSB1", "LSB4"};
 
     @TempDir public File tempDir;
 
-    private Embed embed;
-    private Extract extract;
+    private static Stream<Arguments> provideParameterCombinations() {
+        return Stream.of(SUPPORTED_STEGANOGRAPHY_METHODS)
+                .flatMap(
+                        steganographyMethod ->
+                                Stream.of(CryptographyAlgorithm.values())
+                                        .flatMap(
+                                                cryptographyAlgorithm ->
+                                                        Stream.of(CryptographyMode.values())
+                                                                .map(
+                                                                        cryptographyMode ->
+                                                                                Arguments.of(
+                                                                                        steganographyMethod,
+                                                                                        cryptographyAlgorithm,
+                                                                                        cryptographyMode))));
+    }
 
-    @Test
-    public void appTestLSB1() throws IOException {
+    @ParameterizedTest
+    @ValueSource(strings = {"LSB1", "LSB4"})
+    public void appTestNoCrypto(String steganographyMethod) throws IOException {
 
         String hiddenFilePath = tempDir.getAbsolutePath() + "/hidden.bmp";
         String outputFilePath = tempDir.getAbsolutePath() + "/output";
 
         // Embed
-        embed = new Embed();
-        embed.setInput(messageFilePath);
-        embed.setCover(coverFilePath);
+        Embed embed = new Embed();
+        embed.setInput(MESSAGE_FILE_PATH);
+        embed.setCover(COVER_FILE_PATH);
         embed.setOutput(hiddenFilePath);
-        embed.setSteganographyMethod("LSB1");
+        embed.setSteganographyMethod(steganographyMethod);
         embed.execute();
 
         // Extract
-        extract = new Extract();
+        Extract extract = new Extract();
         extract.setCover(hiddenFilePath);
         extract.setOutput(outputFilePath);
-        extract.setSteganographyMethod("LSB1");
+        extract.setSteganographyMethod(steganographyMethod);
         extract.execute();
 
         // Compare
         boolean areEqual =
                 Files.mismatch(
-                                new File(messageFilePath).toPath(),
+                                new File(MESSAGE_FILE_PATH).toPath(),
                                 new File(outputFilePath + ".java").toPath())
                         == -1;
         Assertions.assertTrue(areEqual);
     }
 
-    @Test
-    public void appTestLSB4() throws IOException {
-
+    @ParameterizedTest
+    @MethodSource("provideParameterCombinations")
+    public void appTestCrypto(
+            String steganographyMethod,
+            CryptographyAlgorithm cryptographyAlgorithm,
+            CryptographyMode cryptographyMode)
+            throws IOException {
         String hiddenFilePath = tempDir.getAbsolutePath() + "/hidden.bmp";
         String outputFilePath = tempDir.getAbsolutePath() + "/output";
 
         // Embed
-        embed = new Embed();
-        embed.setInput(messageFilePath);
-        embed.setCover(coverFilePath);
+        Embed embed = new Embed();
+        embed.setInput(MESSAGE_FILE_PATH);
+        embed.setCover(COVER_FILE_PATH);
         embed.setOutput(hiddenFilePath);
-        embed.setSteganographyMethod("LSB4");
+        embed.setSteganographyMethod(steganographyMethod);
+        embed.setCryptography(new Cryptography(cryptographyAlgorithm, cryptographyMode, PASSWORD));
         embed.execute();
 
         // Extract
-        extract = new Extract();
+        Extract extract = new Extract();
         extract.setCover(hiddenFilePath);
         extract.setOutput(outputFilePath);
-        extract.setSteganographyMethod("LSB4");
+        extract.setSteganographyMethod(steganographyMethod);
+        extract.setCryptography(
+                new Cryptography(cryptographyAlgorithm, cryptographyMode, PASSWORD));
         extract.execute();
 
         // Compare
         boolean areEqual =
                 Files.mismatch(
-                                new File(messageFilePath).toPath(),
+                                new File(MESSAGE_FILE_PATH).toPath(),
                                 new File(outputFilePath + ".java").toPath())
                         == -1;
         Assertions.assertTrue(areEqual);
--- src/test/java/ar/edu/itba/cripto/cryptography/CryptographyTest.java
+++ src/test/java/ar/edu/itba/cripto/cryptography/CryptographyTest.java
@@ -1,28 +1,38 @@
 package ar.edu.itba.cripto.cryptography;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
 
 public class CryptographyTest {
 
-    @Test
-    public void test() {
-        for (CryptographyAlgorithm algorithm : CryptographyAlgorithm.values()) {
-            for (CryptographyMode mode : CryptographyMode.values()) {
-                Cryptography cryptography = new Cryptography(algorithm, mode, "password");
+    private static Stream<Arguments> provideCryptoCombinations() {
+        return Stream.of(CryptographyAlgorithm.values())
+                .flatMap(
+                        algorithm ->
+                                Stream.of(CryptographyMode.values())
+                                        .map(mode -> Arguments.of(algorithm, mode)));
+    }
 
-                String message =
-                        "Hello, World!, this message is going to be encrypted and decrypted by the following algorithm: "
-                                + algorithm
-                                + " using the following mode: "
-                                + mode;
+    @ParameterizedTest
+    @MethodSource("provideCryptoCombinations")
+    public void test(CryptographyAlgorithm algorithm, CryptographyMode mode) {
 
-                byte[] encrypted = cryptography.encrypt(message.getBytes());
-                byte[] decrypted = cryptography.decrypt(encrypted);
+        Cryptography encryptCryptography = new Cryptography(algorithm, mode, "password");
+        Cryptography decryptCryptography = new Cryptography(algorithm, mode, "password");
 
-                Assertions.assertEquals(message, new String(decrypted));
-            }
-        }
+        String message =
+                "Hello, World!, this message is going to be encrypted and decrypted by the following algorithm: "
+                        + algorithm
+                        + " using the following mode: "
+                        + mode;
+
+        byte[] encrypted = encryptCryptography.encrypt(message.getBytes());
+        byte[] decrypted = decryptCryptography.decrypt(encrypted);
+
+        Assertions.assertEquals(message, new String(decrypted));
     }
 }
